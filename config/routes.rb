ab57497ced569ded105185764f8774a727dc8673
Jorge Cisneros <fraxito@gmail.com>
2015-01-21 19:59:39 +0100

    Miercoles: añadido Sass

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,9 @@ Rails.application.routes.draw do
   
   get 'usuarios/:nombre' => 'usuarios#index', as:'lista'
   
+  get 'inicio' => 'paginas_estaticas#inicio'
   
+  get 'ayuda' => 'paginas_estaticas#ayuda'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
